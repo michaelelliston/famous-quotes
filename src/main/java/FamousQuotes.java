e9ccc48e3d0d49422e34dbcf096d3e30a5e6debe
Michael <michael.elliston95@gmail.com>
2025-10-06 14:12:44 -0500

    adjusted formatting, added quote crediting

--- src/main/java/FamousQuotes.java
+++ src/main/java/FamousQuotes.java
@@ -6,25 +6,24 @@ public class FamousQuotes {
         String input;
         int selection;
         // Creating a String array called quotes with 3 values.
-        String[] quotes = {"That wizard came from the Moon.", "I am a wall. And walls don't move. Because walls don't care.", "I don't have time to explain why I don't have time to explain."};
+        String[] quotes = {"\"That wizard came from the Moon.\" - Ghost", "\"I am a wall. And walls don't move. Because walls don't care.\" - Saint-14", "\"I don't have time to explain why I don't have time to explain.\" - Elsie Bray"};
 
         do {
-
             System.out.print("Please input a number from 1 to 3 to select a quote, or input 4 for a random quote: "); // Using myScanner to read user input and make a selection
             input = myScanner.nextLine();
             selection = Integer.parseInt(input);
 
             try {
                 if (selection > 0 && selection < 4) {
-                    System.out.println("Here is your selected quote: \"" + quotes[selection - 1] + '"'); // Prints the selected quote
+                    System.out.println("Here is your selected quote: " + quotes[selection - 1] + '"'); // Prints the selected quote
                 } else if (selection == 4) {
-                    System.out.println("Here is a random quote: \"" + quotes[(int) (Math.random() * 3)]);
+                    System.out.println("Here is a random quote: " + quotes[(int) (Math.random() * 3)]);
                 }
-            } catch (
-                    ArrayIndexOutOfBoundsException e) {            // Catches a number input out of range and prints this message
+            } catch (ArrayIndexOutOfBoundsException e) {            // Catches a number input out of range and prints this message
                 System.out.println("The number you entered was out of the requested range.");
                 e.printStackTrace();
             }
+
             System.out.print("Would you like to see another quote? 1 for yes, 2 for no: "); // Prompting to loop or not
 
             input = myScanner.nextLine();
