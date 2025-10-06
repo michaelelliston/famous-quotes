1a047f82d0d8665bfc20abce20c0af6fa0a9fef6
Michael <michael.elliston95@gmail.com>
2025-10-06 14:00:26 -0500

    fixed syntax

--- src/main/java/FamousQuotes.java
+++ src/main/java/FamousQuotes.java
@@ -5,7 +5,7 @@ public class FamousQuotes {
         Scanner myScanner = new Scanner(System.in);
         String input = "";
         int selection = 0;
-        // Creating a String array called Quotes with 3 values.
+        // Creating a String array called quotes with 3 values.
         String[] quotes = {"That wizard came from the Moon.", "I am a wall. And walls don't move. Because walls don't care.", "I don't have time to explain why I don't have time to explain."};
 
         do {
@@ -17,11 +17,11 @@ public class FamousQuotes {
             try {
                 if (selection > 0 && selection < 4) {
                     System.out.println("Here is your selected quote: \"" + quotes[selection - 1] + '"'); // Prints the selected quote
-                }
-                else if (selection == 4) {
+                } else if (selection == 4) {
                     System.out.println("Here is a random quote: \"" + quotes[(int) (Math.random() * 3)]);
                 }
-            } catch (ArrayIndexOutOfBoundsException e) {            // Catches a number input out of range and prints this message
+            } catch (
+                    ArrayIndexOutOfBoundsException e) {            // Catches a number input out of range and prints this message
                 System.out.println("The number you entered was out of the requested range.");
                 e.printStackTrace();
             }
